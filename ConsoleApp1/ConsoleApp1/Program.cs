ff0636b11049a2345088a8ec1a456163eeb9aa88
Adryano da Costa Celestino Chavier <adryano.chavier@senac.br>
2024-04-26 09:05:01 -0300

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,113 @@
+﻿using System.Globalization;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //double x = 10.35784;
+            //int y = 32;
+            //string z = "Maria";
+            //char w = 'F';
+            //Console.Write("Olá mundo!");
+            //Console.WriteLine("Bom dia!");
+            //Console.WriteLine("Até mais.");
+            //Console.WriteLine();
+            //Console.WriteLine(x);
+            //Console.WriteLine(x.ToString("F2"));
+            //Console.WriteLine(x.ToString("F4"));
+            //Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+            //Console.WriteLine();
+            //Console.WriteLine("RESULTADO = " + x);
+            //Console.WriteLine("O valor do troco é " + x + " reais");
+            //Console.WriteLine("O valor do troco é " + x.ToString("F2") + " reais");
+            //Console.WriteLine();
+            //Console.WriteLine("A paciente " + z + " tem " + y + " anos e seu sexo é: " + w);
+            //Console.ReadLine();
+
+            //Processamento de dados, Casting
+
+            //Exemplo 1
+
+            //int x, y;
+            //x = 5;
+            //y = 2 * x;
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);   
+            //Console.ReadLine();
+
+
+            //Exemplo 2
+
+            //int x;
+            //double y;
+            //x = 5;
+            //y = 2 * x;
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+
+            //Exemplo 3
+
+            //Calculando a area de um trapezio
+
+            //double b, B, h, area;
+            //b = 6.0;
+            //B = 8.0;
+            //h = 5.0;
+            //area = (b + B) / 2.0 * h;
+            //Console.WriteLine(area);
+
+            //float b, B, h,area;
+            //b = 6f;
+            //B = 8f;
+            //h = 5f;
+            //area = (b + B) / 2f * h;
+            //Console.WriteLine(area);
+
+
+            //Exemplo 4 
+            //int a, b;
+            //double resultado;
+            //a = 5;
+            //b = 2;
+            //resultado = (double)a / b; //Casting
+            //Console.WriteLine(resultado);
+
+            //Exemplo 5
+
+            //double a;
+            //int b;
+            //a = 5.0;
+            //b = (int)a; //Casting
+            //Console.WriteLine(b);
+
+            //Entrada de dados
+            //string frase = Console.ReadLine();
+            //string x = Console.ReadLine();
+            //string y = Console.ReadLine();
+            //string i = Console.ReadLine();
+
+            //Console.WriteLine("Você digitou " + frase);
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(i);
+
+
+            //Split
+            // VETOR []
+            //string s = Console.ReadLine();
+            //string[] vet = s.Split(' '); // recortando variavel depois do espaço em branco
+            //string p1 = vet[0]; //recebe a posição zero do vetor
+            //string p2 = vet[1];
+            //string p3 = vet[2];
+            //Console.WriteLine(p1);
+            //Console.WriteLine(p2);
+            //Console.WriteLine(p3);
+
+
+        }
+    }
+}
